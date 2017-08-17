e6ca49b2ebbb6b976ba812c612ac8628b06c1302
Eric Fu <rekeiri@gmail.com>
2017-08-17 16:19:52 -0400

    Cleaned up code by removing lambda expressions bound to buttons, and instead just referenced instance variables inside functions as opposed to passing as arguments.

--- megfilename.py
+++ megfilename.py
@@ -12,9 +12,8 @@ class GUI:
         master.title("String Generator")
         self.initialize_variables(master, dpbox1, dpbox2, dpbox3, pat)
         self.format_layout(master)
-    # functions/methods
 
-        #initializes objects and variables
+    #initializes objects and variables
     def initialize_variables(self, master, dpbox1, dpbox2, dpbox3, pat):
         self.mainframe = ttk.Frame(master, padding=(20, 20, 20, 20))
         self.name = self.read_file2(pat).split("/")[1]
@@ -31,7 +30,7 @@ class GUI:
         self.name_label = ttk.Label(self.mainframe, text="Patient Name: " + self.name)
         self.num_menu_var = StringVar()
         self.num_menu_var2 = StringVar()
-        self.current_index = [0]
+        self.current_index = 0
         self.num_menu_var.set("Current Index: 0")
         self.num_label = ttk.Label(self.mainframe, textvariable=self.num_menu_var)
         self.loc_label = ttk.Label(self.mainframe, text="Location: ")
@@ -45,7 +44,6 @@ class GUI:
             self.num_menu_list = list(range(1, self.num_limit + 1))
         except:
             self.num_menu_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
         self.num_menu_list = ['Autopick'] + self.num_menu_list
         self.num_menu_var2.set(self.num_menu_list[0])
         self.num_menu = OptionMenu(self.mainframe, self.num_menu_var2, *self.num_menu_list)
@@ -64,21 +62,6 @@ class GUI:
         self.protocol_list = ["Choose Protocol", "Functional Meg"]
         self.protocol_var.set(self.protocol_list[0])
         self.protocol_menu = OptionMenu(self.mainframe, self.protocol_var, *self.protocol_list)
-        self.exec_protocol_button = ttk.Button(self.mainframe, text="Start Protocol", command=self.protocol)
-        self.reset_button = ttk.Button(self.mainframe, text='Reset',
-                                       command=lambda: self.reset(self.first_menu_var, self.num_menu_var2,
-                                                                  self.user_text, self.loc_menu_var,
-                                                                  self.eeg_meg_var, self.output1_text,
-                                                                  self.output2_text, self.first_menu_list,
-                                                                  self.num_menu_list, self.loc_menu_list,
-                                                                  self.eeg_meg_list))
-        self.combine_button = ttk.Button(self.mainframe, text='New String',
-                                         command=lambda: self.combine(self.first_menu_var, self.num_menu_var,
-                                                                      self.num_menu_var2, self.user_text,
-                                                                      self.loc_menu_var,
-                                                                      self.eeg_meg_var, self.output1_text,
-                                                                      self.output2_text, self.current_index))
-        self.combine_button.bind("<Button-1>", self.callback)
         self.exit_button = ttk.Button(self.mainframe, text='Exit', command=lambda: self.exit_func(master))
         self.output1_label = ttk.Label(self.mainframe, text='SEF/MEF File String: ')
         self.output1_text = Text(self.mainframe, width=60, height=2, font=("Times New Roman", 16))
@@ -86,8 +69,13 @@ class GUI:
         self.output2_text = Text(self.mainframe, width=60, height=2, font=("Times New Roman", 16))
         self.output3_label = ttk.Label(self.mainframe, text='Additional Comments: ')
         self.output3_text = Text(self.mainframe, width=60, height=2, font=("Times New Roman", 16))
+        # initialize buttons
+        self.exec_protocol_button = ttk.Button(self.mainframe, text="Start Protocol", command=self.protocol)
+        self.reset_button = ttk.Button(self.mainframe, text='Reset', command = self.reset())
+        self.combine_button = ttk.Button(self.mainframe, text='New String', command= self.combine)
+        self.combine_button.bind("<Button-1>", self.callback_combine_button)
 
-        #configures the layout(spacing, placement, size, etc.)
+    #configures the layout(spacing, placement, size, etc.)
     def format_layout(self, master):
         self.master.columnconfigure(1, weight=1)
         self.master.rowconfigure(1, weight=1)
@@ -124,10 +112,9 @@ class GUI:
         self.output3_label.grid(row=7, column=1, sticky=(N, W, S), padx=10, pady=15)
         self.output3_text.grid(row=7, column=2, columnspan=4, sticky=(N, E, S, W), pady=15)
 
-
-
-    def callback(self, event):
-        self.update_label(self.num_menu_var, self.num_menu_var2, self.current_index)
+    #callback for combine_button
+    def callback_combine_button(self, event):
+        self.update_label()
 
         # returns a list of the words in the text file
     def read_file(self, path):
@@ -138,52 +125,61 @@ class GUI:
         f = open(path, "r")
         return f.read()
 
+    #allows exit button to exit program
     def exit_func(self, root):
         root.destroy()
 
         # executes before combine
-    def update_label(self, tkvar, tkvar2, current_index):
-        prefix = "Current Index: "
-        if tkvar2.get() != "Autopick":
-            if (current_index[0] == int(tkvar2.get())):
-                current_index[0] = current_index[0] + 1
-                tkvar2.set(current_index[0])
-            else:
-                current_index[0] = int(tkvar2.get())
-        else:
-            current_index[0] = current_index[0] + 1
-        tkvar.set(prefix + str(current_index[0]))
 
-    def combine(self, dbv1, dbv2_1, dbv2, entryvar, dbv3, dbv4, text1, text2, current_index):
-        text1.delete(1.0, END)  # since text is multi-line, 1.0 means line 1, char 0
-        text2.delete(1.0, END)
-        list_strings = [dbv1.get(), dbv2.get(), entryvar.get(), dbv3.get(), dbv4.get()]
+    #updates the label that displays the current index
+    def update_label(self):
+        prefix = "Current Index: "
+        if self.num_menu_var2.get() != "Autopick":
+            if (self.current_index == int(self.num_menu_var2.get())):
+                self.current_index = self.current_index + 1
+                self.num_menu_var2.set(self.current_index)
+            else:
+                self.current_index = int(self.num_menu_var2.get())
+        else:
+            self.current_index = self.current_index + 1
+        self.num_menu_var.set(prefix + str(self.current_index))
+
+    #function for combine button that concatenates the options chosen
+    def combine(self):
+        self.output1_text.delete(1.0, END)  # since text is multi-line, 1.0 means line 1, char 0
+        self.output2_text.delete(1.0, END)
+        #creates a list of all options selected
+        list_strings = [self.first_menu_var.get(), self.num_menu_var2.get(), self.user_text.get(), self.loc_menu_var.get(), self.eeg_meg_var.get()]
+        #removes an option if it is NULL
         for i in range(len(list_strings) - 1, -1, -1):
             if list_strings[i] in ["NULL", ""]:
                 del list_strings[i]
+        #if autopick is chosen for the index, make it new string choose
         for j in range(len(list_strings)):
             if list_strings[j] in ['Autopick']:
-                list_strings[j] = str(current_index[0])
-        if (dbv1.get() in ["SEF", "MEF"]):
-            text1.insert(1.0, "_".join(list_strings) + "_avg")
-        text2.insert(1.0, "_".join(list_strings) + "_raw")
+                list_strings[j] = str(self.current_index)
+        #if first option is mef/sef, concatenate _avg instead of raw on the end.
+        if (self.first_menu_var.get() in ["SEF", "MEF"]):
+            self.output1_text.insert(1.0, "_".join(list_strings) + "_avg")
+        self.output2_text.insert(1.0, "_".join(list_strings) + "_raw")
 
-    def reset(self, dbv1, dbv2, entryv, dbv3, dbv4, text1, text2, dbv1_list, dbv2_list, dbv3_list, dbv4_list):
-        dbv1.set(dbv1_list[0])
-        dbv2.set(dbv2_list[0])
-        dbv3.set(dbv3_list[0])
-        dbv4.set(dbv4_list[0])
-        entryv.set(self.initials)
-        text1.delete(1.0, END)
-        text2.delete(1.0, END)
-        self.current_index = [0]
-        self.num_menu_var.set("Current Index: 0")
-        # reset the protocol menu
+    def reset(self):
+        #reset the menus to the first item in the list
+        self.first_menu_var.set(self.first_menu_list[0])
+        self.num_menu_var2.set(self.num_menu_list[0])
+        self.loc_menu_var.set(self.loc_menu_list[0])
+        self.eeg_meg_var.set(self.eeg_meg_list[0])
         self.protocol_var.set(self.protocol_list[0])
+        #reset the user text to the initials from the patient file
+        self.user_text.set(self.initials)
+        #reset the text outputs
+        self.output1_text.delete(1.0, END)
+        self.output2_text.delete(1.0, END)
+        self.output3_text.delete(1.0, END)
+        self.current_index = 0
+        self.num_menu_var.set("Current Index: 0")
         # reset the protocol button command
         self.exec_protocol_button.configure(text="Start Protocol", command=self.protocol)
-        # reset the extra comments
-        self.output3_text.delete(1.0, END)
 
     # allows for future protocols to be added and edited.
     def protocol(self):
@@ -204,9 +200,7 @@ class GUI:
         try:
             curr_line = lines.pop(0)
         except:
-            self.reset(self.first_menu_var, self.num_menu_var2, self.user_text, self.loc_menu_var,
-                       self.eeg_meg_var, self.output1_text, self.output2_text, self.first_menu_list,
-                       self.num_menu_list, self.loc_menu_list, self.eeg_meg_list)
+            self.reset()
             return
         curr_line = curr_line.split("/")
         for i in range(len(curr_line) - 1, -1, -1):
